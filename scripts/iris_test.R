ca7ca9f569656ea4cbf516f15b202f54907f02e0
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-05-20 15:59:13 +0100

    Improve iris test script.

--- scripts/iris_test.R
+++ scripts/iris_test.R
@@ -1,6 +1,7 @@
 ##
 #
 # Test the Symbolic PCA for intervals with the iris data.
+# Plot intervals for two variables and for two PCs, as well as the intervals for those variables alone.
 #
 ##
 
@@ -11,11 +12,22 @@ library(ggplot2)
 data(iris)
 set.seed(73) # for reproducibility
 
-## Set symbolic variables here
+## Plot the conventional variables conv[1] and conv[2] when ordered by variance.
+## Plot the PCs pc[1] and pc[2] when ordered by variance.
+conv <- c(1, 3)
+pc <- c(1, 3)
 
+## Set symbolic variables here
 K <- 2                       # the symbolic model to be used.
 algebra <- "vector space"    # the interval algebra to be used.
 restriction <- "orthogonal"  # the type of restriction to enforce.
+
+# Helper function to set the axis of the plots
+stretch.axis <- function(axis) {
+    axis.range <- axis[2] - axis[1]
+    delta <- 0.1*axis.range
+    c(axis[1]-delta, axis[2]+delta)
+}
 
 ## Start by grouping the data inside each species to create some fake intervals.
 
@@ -53,23 +65,23 @@ for (i in 1:n_groups) {
 }
 
 # Prepare plotting area
-par(mfrow=c(2,2))
+par(mfrow=c(2,3))
 
 # Find the 2 columns with greatest variance and plot their intervals.
 sigma.k <- cov.k(C, R, K)
 vars.k <- diag(sigma.k)
-cols <- sort.int(vars.k, decreasing = TRUE, index.return = TRUE)$ix[1:2]
-print("The columns with greatest variance are")
-print(colnames(C)[cols])
+cols <- sort.int(vars.k, decreasing = TRUE, index.return = TRUE)$ix[conv]
+xaxis <- stretch.axis(c(
+    min(C[, cols[1]]-R[, cols[1]]),
+    max(C[, cols[1]]+R[, cols[1]])
+))
+yaxis <- stretch.axis(c(
+    min(C[, cols[2]]-R[, cols[2]]),
+    max(C[, cols[2]]+R[, cols[2]])
+))
 plot(
-    c(
-        min(C[, cols[1]]-R[, cols[1]])-2,
-        max(C[, cols[1]]+R[, cols[1]])+2
-    ),
-    c(
-        min(C[, cols[2]]-R[, cols[2]])-2,
-        max(C[, cols[2]]+R[, cols[2]])+2
-    ),
+    xaxis,
+    yaxis,
     xlab = colnames(C)[cols[1]],
     ylab = colnames(C)[cols[2]],
     main = paste("Intervals for", colnames(C)[cols[1]], "and", colnames(C)[cols[2]]),
@@ -81,25 +93,43 @@ rect(
     ytop = C[, cols[2]]+R[, cols[2]],
     border = c("red", "blue", "green")[match(species, levels(species))]
 )
-# Now plot the intervals of the variable with greatest variance.
+# Now plot the intervals of the variable conv[1].
+plot_height <- max(R[, cols[1]]) - min(R[, cols[1]])
 plot(
+    xaxis,
     c(
-        min(C[, cols[1]]-R[, cols[1]])-2,
-        max(C[, cols[1]]+R[, cols[1]])+2
-    ),
-    c(
-        min(R[, cols[1]])-2,
-        max(R[, cols[1]])+2
+        min(R[, cols[1]]),
+        max(R[, cols[1]])
     ),
     xlab = colnames(C)[cols[1]],
     ylab = "Range",
-    main = paste("Intervals for", colnames(C)[cols[1]], "with y depending on the range"),
+    main = paste("Intervals for", colnames(C)[cols[1]], "with y = range"),
 )
 rect(
     xleft = C[, cols[1]]-R[, cols[1]],
-    ybottom = R[, cols[1]]-0.04,
+    ybottom = R[, cols[1]]-plot_height/100,
     xright = C[, cols[1]]+R[, cols[1]],
-    ytop = R[, cols[1]]+0.04,
+    ytop = R[, cols[1]]+plot_height/100,
+    density = 50,
+    col = c("red", "blue", "green")[match(species, levels(species))]
+)
+# Now plot the intervals of the variable conv[2].
+plot_height <- max(R[, cols[2]]) - min(R[, cols[2]])
+plot(
+    yaxis,
+    c(
+        min(R[, cols[2]]),
+        max(R[, cols[2]])
+    ),
+    xlab = colnames(C)[cols[2]],
+    ylab = "Range",
+    main = paste("Intervals for", colnames(C)[cols[2]], "with y = range"),
+)
+rect(
+    xleft = C[, cols[2]]-R[, cols[2]],
+    ybottom = R[, cols[2]]-plot_height/100,
+    xright = C[, cols[2]]+R[, cols[2]],
+    ytop = R[, cols[2]]+plot_height/100,
     density = 50,
     col = c("red", "blue", "green")[match(species, levels(species))]
 )
@@ -110,46 +140,66 @@ result <- sym.pca(C, R, K, interval.algebra = "vector space", restriction = "ort
 newC <- C %*% result$vectors
 newR <- R %*% result$vectors
 absR <- abs(newR)
-# Plot the rectangles for the first two PCAs
+# Plot the rectangles for the PCs in pc
+xaxis <- stretch.axis(c(
+    min(newC[,pc[1]]-absR[,pc[1]]),
+    max(newC[,pc[1]]+absR[,pc[1]])
+))
+yaxis <- stretch.axis(c(
+    min(newC[,pc[2]]-absR[,pc[2]]),
+    max(newC[,pc[2]]+absR[,pc[2]])
+))
 plot(
-    c(
-        min(newC[,1]-absR[,1])-2,
-        max(newC[,1]+absR[,1])+2
-    ),
-    c(
-        min(newC[,2]-absR[,2])-2,
-        max(newC[,2]+absR[,2])+2
-    ),
-    xlab = "1st PCA",
-    ylab = "2nd PCA",
-    main = "Intervals of the 1st and 2nd PCAs.",
+    xaxis,
+    yaxis,
+    xlab = "pc[1]",
+    ylab = "pc[2]nd PC",
+    main = paste("Intervals for PCs", pc[1], "and", pc[2]),
 )
 rect(
-    xleft = newC[, 1]-newR[, 1],
-    ybottom = newC[, 2]-newR[, 2],
-    xright = newC[, 1]+newR[, 1],
-    ytop = newC[, 2]+newR[, 2],
+    xleft = newC[, pc[1]]-newR[, pc[1]],
+    ybottom = newC[, pc[2]]-newR[, pc[2]],
+    xright = newC[, pc[1]]+newR[, pc[1]],
+    ytop = newC[, pc[2]]+newR[, pc[2]],
     border = c("red", "blue", "green")[match(species, levels(species))]
 )
-# Now only plot the 1st PCA with y depending on the range of the interval.
+# Now only plot the pc[1] with y depending on the range of the interval.
+plot_height <- max(newR[, pc[1]]) - min(newR[, pc[1]])
 plot(
+    xaxis,
     c(
-        min(newC[,1]-absR[,1])-2,
-        max(newC[,1]+absR[,1])+2
-    ),
-    c(
-        min(newR[,1]),
-        max(newR[,1])
+        min(newR[,pc[1]]),
+        max(newR[,pc[1]])
     ),
     xlab = "Intervals",
     ylab = "Ranges",
-    main = "Intervals of the 1st PCA with y depending only on the range.",
+    main = paste("Intervals of PC", pc[1], "with y = range."),
 )
 rect(
-    xleft = newC[, 1]-newR[, 1],
-    ybottom = newR[, 1]-0.02,
-    xright = newC[, 1]+newR[, 1],
-    ytop = newR[, 1]+0.02,
+    xleft = newC[, pc[1]]-newR[, pc[1]],
+    ybottom = newR[, pc[1]]-plot_height/100,
+    xright = newC[, pc[1]]+newR[, pc[1]],
+    ytop = newR[, pc[1]]+plot_height/100,
+    density = 50,
+    col = c("red", "blue", "green")[match(species, levels(species))]
+)
+# Now only plot the pc[2] with y = range.
+plot_height <- max(newR[, pc[2]]) - min(newR[, pc[2]])
+plot(
+    yaxis,
+    c(
+        min(newR[,pc[2]]),
+        max(newR[,pc[2]])
+    ),
+    xlab = "Intervals",
+    ylab = "Ranges",
+    main = paste("Intervals of PC", pc[2], "with y = range."),
+)
+rect(
+    xleft = newC[, pc[2]]-newR[, pc[2]],
+    ybottom = newR[, pc[2]]-plot_height/100,
+    xright = newC[, pc[2]]+newR[, pc[2]],
+    ytop = newR[, pc[2]]+plot_height/100,
     density = 50,
     col = c("red", "blue", "green")[match(species, levels(species))]
 )
